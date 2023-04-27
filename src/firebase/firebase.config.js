7b3f5a29faecec395fc9042a06eb1ae07aa2073c
Shaikat roy <shaikatroy258@gmail.com>
2023-04-28 00:47:32 +0600

    sidebar added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA2y5eOW91vxTv1yaWdn7PC5tcq1xYfGL4",
+  authDomain: "the-news-dragon-client-13485.firebaseapp.com",
+  projectId: "the-news-dragon-client-13485",
+  storageBucket: "the-news-dragon-client-13485.appspot.com",
+  messagingSenderId: "513315864007",
+  appId: "1:513315864007:web:7a99d463770c95c3776de2"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
---
+++ src/pages/Shared/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <div className='text-center'>
+            <p>copyright warning from Dragon News</p>
+        </div>
+    );
+};
+
+export default Footer;
