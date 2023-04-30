2cd7899b9ddaa22a47dce9674917d99c9033d70e
Shaikat roy <shaikatroy258@gmail.com>
2023-04-30 15:57:34 +0600

    login register complete

--- src/pages/Shared/Header/Header.jsx
+++ src/pages/Shared/Header/Header.jsx
@@ -1,15 +1,10 @@
-import React, { useContext } from 'react';
+import React from 'react';
 import logo from '../../../assets/logo.png'
 import moment from 'moment';
-import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 import Marquee from "react-fast-marquee";
-import { Link } from 'react-router-dom';
-import { AuthContext } from '../../../providers/AuthProviders';
-import { FaUserCircle } from 'react-icons/fa';
 
 const Header = () => {
-
-    const {user} = useContext(AuthContext);
 
     return (
         <Container className='mt-4'>
@@ -24,31 +19,6 @@ const Header = () => {
                 Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...
                 </Marquee>
             </div>
-            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
-                <Container>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav className="mx-auto">
-                            <Nav.Link href="#features">
-                                <Link to="/">Home</Link>
-                            </Nav.Link>
-                            <Nav.Link href="#pricing">About</Nav.Link>
-                            <Nav.Link href="#pricing">Career</Nav.Link>
-                        </Nav>
-                        <Nav>
-                           {user && <Nav.Link href="#deets"><FaUserCircle style={{fontSize: '2rem'}}></FaUserCircle></Nav.Link>}
-                            <Nav.Link eventKey={2} href="#memes">
-                            { user ? 
-                                <Button variant="dark">Logout</Button> :
-                            <Link to="/login">
-                                <Button variant="dark">Login</Button>
-                            </Link>
-                            }
-                            </Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
         </Container>
     );
 };
---
+++ src/pages/Shared/Terms/Terms.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Terms = () => {
+    return (
+        <Container>
+            <h2>This terms and conditions</h2>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam tempore illum et, adipisci harum expedita sapiente beatae? Natus molestias totam nulla aperiam quisquam doloremque quaerat perspiciatis voluptates ad ex nostrum corrupti quo officia quia delectus aspernatur quidem veritatis odio, placeat quis! Atque beatae nisi magni voluptatem. Iusto laudantium velit a modi voluptatibus quidem? Nobis laudantium eligendi quia,
+             nostrum porro quam quos corporis nemo, maxime, obcaecati enim sed 
+             dolorum ratione? Totam fuga necessitatibus modi explicabo molestias animi sint, expedita praesentium illo delectus? Quis deleniti vitae, iste incidunt sequi perferendis veritatis itaque odit tempore quam dolore animi fugiat odio harum sunt recusandae quaerat laudantium libero nihil aspernatur ea voluptatum. Illo reiciendis mollitia suscipit quae aut quaerat placeat obcaecati nam! Autem itaque beatae accusamus similique ullam dolorum ut, animi vel molestias quisquam quasi ipsa commodi asperiores ex aliquam quod ea? Iste assumenda unde dolores neque nesciunt rem consectetur aperiam incidunt architecto. Quasi, voluptate, asperiores distinctio deleniti nemo consequatur perferendis enim rerum hic harum sit quisquam consequuntur ducimus est aperiam, modi dolore incidunt deserunt cum nam expedita officia labore? Fugiat nostrum repellendus minus amet, modi porro quae? Repellendus quae earum praesentium neque tenetur. Deleniti dicta architecto rerum est asperiores corrupti voluptatum obcaecati commodi porro!
+            </p>
+            <p>Go back to <Link to="/register">Register</Link></p>
+        </Container>
+    );
+};
+
+export default Terms;
